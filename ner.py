9657b41ce6de2311f75ba08652e4c66ac46d8f34
ScobraCk <scobrack@gmail.com>
2023-09-13 12:32:41 +0900

    added test code

--- ner.py
+++ ner.py
@@ -15,4 +15,4 @@ def get_predict(text):
     return predict(pred_config)
 
 if __name__ == "__main__":
-    print(get_predict('안녕하세요 단국대학교 학생 홍길동입니다.'))
+    print(get_predict('서울시 강남구에 사는 직장인 홍길동.'))
